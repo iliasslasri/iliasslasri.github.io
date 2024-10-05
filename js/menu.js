49e18ce646905713acd19277dba71c8decd2ab5d
Iliass Lasri <iliass.lasri@outlook.com>
2024-10-05 19:37:33 +0200

    cst

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,3 @@
-/*************************************************************************
- * For loading the menu.
- * The menu will not work if you open the file directly rather than on a web server.
- *************************************************************************/
-
 (function () {
   "use strict";
 
