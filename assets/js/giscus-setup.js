6ff7a50fc41b4341f5d4a4adbff179f2755e51b1
iliasslasri <116740713+iliasslasri@users.noreply.github.com>
2025-12-16 20:49:36 +0000

    Deploying to gh-pages from @ iliasslasri/iliasslasri.github.io@66b82ac06c6d87615b58f0540455d96b203dad93 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "iliasslasri/iliass",
+    "data-repo": "iliasslasri/iliasslasri.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
